d8793ff850199b02e8c2666624ed25e52f48494c
CoRoNAviRusNIk <moasosiskag6@gmail.com>
2025-10-23 18:02:25 +0300

    1

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+fish_add_path /home/kot/.spicetify
+
+# Created by `pipx` on 2025-09-03 22:21:02
+set PATH $PATH /home/kot/.local/bin
